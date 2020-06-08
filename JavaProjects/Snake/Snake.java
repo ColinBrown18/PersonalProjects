3ab515a1b7ca033a736142206111209716c43902
Colin Brown <colinrb121@gmail.com>
2020-06-08 15:29:34 -0400

    fixed contingency error

--- JavaProjects/Snake/Snake.java
+++ JavaProjects/Snake/Snake.java
@@ -1,11 +1,16 @@
-
-
 package Snake;
-class Coordinate {
-    int x, y;
+public class Snake {
 
-    Coordinate(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public static void main(String[] args) {
+        
+    }
+
+    class Coordinate {
+        int x, y;
+    
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
