9049ce68a0e5dd7644fbc728589f6bc7384d58e9
Colin Brown <colinrb121@gmail.com>
2020-06-08 16:08:52 -0400

    test

--- JavaProjects/Snake/Snake.java
+++ JavaProjects/Snake/Snake.java
@@ -1,24 +1,25 @@
 package snake;
+
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
 public class Snake {
 
     public JFrame jframe;
 
+    public static Snake snake;
+
     public Snake() {
+        Dimenstion dim = Toolkit.getDefaultToolkit().getScreenSize();
         jframe = new JFrame("Snake");
         jframe.setVisible(true);
         jframe.setSize(800, 800);
+        jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.width / 2 - jframe.getHeight() / 2);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-        
-    }
-
-    class Coordinate {
-        int x, y;
-    
-        Coordinate(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+        snake = new Snake();
     }
 }
