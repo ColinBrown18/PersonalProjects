8a377dc70ee7e3c27ab3e62a13b0394cb97340cd
Colin Brown <colinrb121@gmail.com>
2020-06-08 15:28:31 -0400

    Basic code added to Snake.java

---
+++ JavaProjects/Snake.java
@@ -0,0 +1,16 @@
+package Snake;
+public class Snake {
+
+    public static void main(String[] args) {
+        
+    }
+
+    class Coordinate {
+        int x, y;
+    
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
