43a0c84b486301836ed0cec6cfd49a8c475b037b
Colin Brown <colinrb121@gmail.com>
2020-06-08 23:44:13 -0400

    Snake updates

--- JavaProjects/Snake/RenderPanel.java
+++ JavaProjects/Snake/RenderPanel.java
@@ -8,14 +8,14 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class RenderPanel extends JPanel {
 
-    public static int curColor = 0;
+    public static Color blue = new Color(255);
     
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(new Color(curColor));
+        g.setColor(blue);
         g.fillRect(0, 0, 800, 700);
-        curColor++;
+        
     }
 }
--- JavaProjects/Snake/Snake.java
+++ JavaProjects/Snake/Snake.java
@@ -1,20 +1,35 @@
 package Snake;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import java.awt.Toolkit;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Point;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class Snake implements ActionListener{
-
+    public static Snake snake;
     public JFrame jframe;
     public RenderPanel renderPanel;
-    public Timer timer = new Timer(20, this);
-    public static Snake snake;
 
+    public Timer timer = new Timer(20, this);
+
+    // Snake construction
+    public ArrayList<Point> snakeBody = new ArrayList<Point>();
+    public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
+    public int ticks = 0, direction = DOWN;
+    public Point head, cherry;
+
+    public Random rand;
+
+    public static void main(final String[] args) {
+        snake = new Snake();
+    }
 
     // This creates the dimensions of the program
     public Snake() {
@@ -27,15 +42,36 @@ public class Snake implements ActionListener{
         jframe.add(renderPanel = new RenderPanel());
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        timer.start();
-    }
+        head = new Point(0, 0);
+        rand = new Random();
+        cherry = new Point(dim.width / SCALE, 0);
 
-    public static void main(final String[] args) {
-        snake = new Snake();
+        timer.start();
     }
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
         renderPanel.repaint();
+        ticks++;
+
+         
+        if (ticks % 10 == 0) {
+            if (direction == UP) {
+                snakeBody.add(new Point(head.x, head.y - 1));
+            } else if (direction == DOWN) {
+                snakeBody.add(new Point(head.x, head.y + 1));
+            } else if (direction == LEFT) {
+                snakeBody.add(new Point(head.x - 1, head.y));
+            } else if (direction == RIGHT) {
+                snakeBody.add(new Point(head.x + 1, head.y));
+            }
+            if (cherry != null) {
+                if (head.equals(cherry)) {
+                    score++;
+                    cherry.setLocation(dim.width / SCALE, dim.height / SCALE);
+                }
+            }
+        }
+        
     }
 }
