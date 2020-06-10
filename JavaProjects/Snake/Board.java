5a26c31ea9e196c46fbaa5d4667f808605895588
Colin Brown <colinrb121@gmail.com>
2020-06-10 19:00:00 -0400

    Moving snake added

--- JavaProjects/Snake/Board.java
+++ JavaProjects/Snake/Board.java
@@ -1,2 +1,32 @@
+package Snake;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class Board extends JPanel {
+
+    public static Color blue = new Color(255);
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(blue);
+        g.fillRect(0, 0, 800, 700);
+        
+        Snake snake = Snake.snake;
+
+        g.setColor(Color.RED);
+        // For loop is iterating through an array
+        for (Point point : snake.snakeBody) {
+            g.fillRect(point.x * snake.SCALE, point.y * snake.SCALE, snake.SCALE, snake.SCALE);
+        }
+        g.fillRect(snake.head.x * snake.SCALE, snake.head.y * snake.SCALE, snake.SCALE, snake.SCALE);
+    }
+}
 
--- JavaProjects/Snake/Snake.java
+++ JavaProjects/Snake/Snake.java
@@ -7,39 +7,44 @@ import java.awt.Toolkit;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.Point;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class Snake implements ActionListener{
+public class Snake implements ActionListener, KeyListener {
     public static Snake snake;
     public JFrame jframe;
-    public RenderPanel renderPanel;
+    public Board board;
 
     public Timer timer = new Timer(20, this);
 
     // Snake construction
     public ArrayList<Point> snakeBody = new ArrayList<Point>();
     public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
-    public int ticks = 0, direction = DOWN;
+    public int ticks = 0, direction = DOWN, score, tailLength = 10;
     public Point head, cherry;
 
     public Random rand;
 
-    public static void main(final String[] args) {
-        snake = new Snake();
-    }
+    public boolean over = false;
+
+    Dimension dim;
+
+  
 
     // This creates the dimensions of the program
     public Snake() {
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        dim = Toolkit.getDefaultToolkit().getScreenSize();
         jframe = new JFrame("Snake");
         jframe.setVisible(true);
         jframe.setSize(800, 700);
         jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.width 
             / 2 - jframe.getHeight() / 2);
-        jframe.add(renderPanel = new RenderPanel());
+        jframe.add(board = new Board());
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         head = new Point(0, 0);
@@ -47,31 +52,92 @@ public class Snake implements ActionListener{
         cherry = new Point(dim.width / SCALE, 0);
 
         timer.start();
+
+        for (int i = 0; i < tailLength; i++) {
+            snakeBody.remove(i);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
-        renderPanel.repaint();
+        board.repaint();
         ticks++;
 
          
-        if (ticks % 10 == 0) {
+        if (ticks % 10 == 0 && head != null && over != true) {
+            // 
+            snakeBody.add(new Point(head.x, head.y));
             if (direction == UP) {
-                snakeBody.add(new Point(head.x, head.y - 1));
-            } else if (direction == DOWN) {
-                snakeBody.add(new Point(head.x, head.y + 1));
-            } else if (direction == LEFT) {
-                snakeBody.add(new Point(head.x - 1, head.y));
-            } else if (direction == RIGHT) {
-                snakeBody.add(new Point(head.x + 1, head.y));
+                if (head.y - 1 > 0) {
+                    head = new Point(head.x, head.y - 1);
+                } else {
+                    over = true;
+                }
+            } if (direction == DOWN) {
+                if (head.y + 1 < dim.height / SCALE) {
+                    head = new Point(head.x, head.y + 1);
+                } else {
+                    over = true;
+                }
+            } if (direction == LEFT) {
+                if (head.x - 1 > 0) {
+                    head = new Point(head.x - 1, head.y);
+                } else {
+                    over = true;
+                }
+            } if (direction == RIGHT) {
+                if (head.x + 1 < dim.width / SCALE) {
+                    head = new Point(head.x + 1, head.y);
+                } else {
+                    over = true;
+                }
             }
+
+            // This removes the snake body from the position it was previously in after it was put in the next position
+            // This creates the visual of it moving accross the board.
+            for (int i = 0; -i < tailLength; i++) {
+                snakeBody.remove(i);
+            }
+            
+            
             if (cherry != null) {
                 if (head.equals(cherry)) {
-                    score++;
+                    score+=10;
+                    tailLength++;
                     cherry.setLocation(dim.width / SCALE, dim.height / SCALE);
                 }
             }
         }
         
+    }
+    public static void main(final String[] args) {
+        snake = new Snake();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int i = e.getKeyCode();
+        if (i == KeyEvent.VK_A && direction != RIGHT) {
+            direction = LEFT;
+        }
+        if (i == KeyEvent.VK_D && direction != LEFT) {
+            direction = RIGHT;
+        }
+        if (i == KeyEvent.VK_W && direction != DOWN) {
+            direction = UP;
+        }
+        if (i == KeyEvent.VK_S && direction != UP) {
+            direction = DOWN;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
     }
 }
