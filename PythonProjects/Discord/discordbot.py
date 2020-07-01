5bb8734ee779f721f8e5de6cde442561aab6655f
Colin Brown <colinrb121@gmail.com>
2020-07-01 13:23:38 -0400

    working output

--- PythonProjects/Discord/discordbot.py
+++ PythonProjects/Discord/discordbot.py
@@ -1,1 +1,10 @@
+import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix='.')
+
+@bot.event
+async def on_ready():
+    print('bot is ready')
+
+bot.run('NzI3OTI5OTY0MjE2NzEzMzA3.Xvy_tQ.le85jQCe6YYDGAotphthKsQujNs')
